5cd9465d8c508c607c876d9d34275932719437c2
Vinicius_siqueira098 <viniv908@gmail.com>
2025-08-04 23:19:06 -0300

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 // Importa o framework Express para criar o servidor HTTP
 const express = require("express");
+const cors = require("cors");
 
 // Importa o módulo de rotas definido no arquivo 'router.js'
 const router  = require("./router");
@@ -8,9 +9,11 @@ const router  = require("./router");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(router); // Usa as rotas definidas no módulo router (middleware)
 
-// Usa as rotas definidas no módulo router (middleware)
-app.use(router);
+
+
 
 // Exporta a aplicação para que possa ser usada em outro lugar (ex: no server.js)
 module.exports = app;
